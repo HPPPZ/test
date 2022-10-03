1ac59fb8fcfb7115cbd71e81dba32bdf5d0aa454
HPPPZ <15094082275@163.com>
2022-10-03 23:26:57 +0800

    侧式代码

---
+++ game1/game1/test.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include"game.h"
+void menu()
+{
+	printf("***********************\n");
+	printf("****1.play  0.exit ****\n");
+	printf("***********************\n");
+}
+void game()
+{
+	char board[ROW][COL] = { 0 };
+	InitBoard(board, ROW, COL);
+	DisplayBoard(board,ROW,COL);
+	char ret = 0;
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board,ROW,COL);
+		DisplayBoard(board, ROW, COL);
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+			break;
+		//电脑下棋
+		ComputerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+			break;
+	}
+	if (ret == '*')
+	{
+		printf("玩家赢了\n");
+	}
+	else if (ret == '#')
+	{
+		printf("电脑赢了\n");
+	}
+	else
+	{
+		printf("平局\n");
+	}
+	DisplayBoard(board, ROW, COL);
+}
+
+void test()
+{
+	int input = 0;
+	do
+	{
+		menu();
+			printf("请选择:>");
+			scanf("%d", &input);
+			switch(input)
+			{
+				case 1:
+					game();
+					break;
+				case 0:
+					printf("退出游戏\n");
+					break;
+				default:
+					printf("选择错误，请重新选择\n");
+					break;
+			}
+	}
+	while(input);
+}
+int main()
+{
+	srand((unsigned int)time(NULL));
+	test();
+	return 0;
+}
